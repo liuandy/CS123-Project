51a2fcd07767625c403d3d413b062c46d722bd20
Andy Liu <andyliu1220@gmail.com>
2013-05-07 15:25:56 -0500

    Removed setwd line

--- prototype.r
+++ prototype.r
@@ -1,4 +1,3 @@
-setwd("C:/Users/nauner/SkyDrive/School/ClassMaterials/Chicago2012-2013/Spring/CMSC123/andyproj/CS123-Project")
 data <- read.table("census-income.data", sep = ",", header =F, na.strings = "NA")
 names(data) <- c("AAGE","ACLSWKR","ADTIND","ADTOCC","AHGA","AHRSPAY","AHSCOL","AMARITL","AMJIND","AMJOCC","ARACE","AREORGN","ASEX","AUNMEM","AUNTYPE","AWKSTAT","CAPGAIN","CAPLOSS","DIVVAL","FILESTAT","GRINREG","GRINST","HHDFMX","HHDREL","MARSUPWT","MIGMTRM1","MIGMTR3","MIGMTR4","MIGSAME","MIGSUN","NOEMP","PARENT","PEFNTVTY","PEMNTVTY","PENATVTY","PRCITSHP","SEOTR","VETQVA","VETYN","WKSWORK","YEAR","INC")
 # Extract only the continuous data to work with for now
