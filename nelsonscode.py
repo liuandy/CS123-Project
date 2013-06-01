9253e7c845a0ca00565053c195214e99f2c5f3f8
Nelson Auner <ne.auner@gmail.com>
2013-06-01 14:12:01 -0500

    finished basic cross-validation results plotter. replace iterators with lists to make use of pyplot.plot(list1,list2) syntax

--- nelsonscode.py
+++ nelsonscode.py
@@ -1,4 +1,4 @@
-#sources: 
+ #sources: 
 
 #Required modules:
 #numpy: a language extension that defines the numerical array and matrix
@@ -17,6 +17,7 @@ import statsmodels.api as sm
 import pylab as pl
 import numpy as np
 import random
+import pylab
 
 
 #Learning pandas:
@@ -34,37 +35,101 @@ print data
 #note that pandas includes a .index part 
 
 #Transfering R to python
-#create a small sample
+#create a small samples
 n = 1000
 rowstokeep = random.sample(data.index,n)
 s = data.ix[rowstokeep]
 colstokeep = ['CLASS','AHRSPAY','WKSWORK','AHGA']
 s = s[colstokeep]
 train_cols = ['AHGA']
-s['intercept'] = 1.0  #manually add in intercept term
-########This returns an error because python does not automatically handle factor variables
-#We must create these variables seperately.
-for elem in s['AHGA'].unique():
-    s[str(elem)] = s['AHGA'] == elem
-	
-y = s[s.columns[0]]
-X = s[s.columns[5:]]
-logit = sm.Logit(s[s.columns[0]], s[s.columns[5:]])
-result = logit.fit()
+s['intercept'] = 1.0  #manually add in intercept term. or not. 
 
-yhat = result.predict() #returns a bunch of values between 0 and 1. 
+#Create factor variables
+for elem in s['AHGA'].unique():
+    s[str(elem)] = s['AHGA'] == elem #A bool for each possible observations
+	
+	
+#Use this procedure to perform logistic regression
+y = s[s.columns[0]]
+X = s[s.columns[6:]]  #educational dummy variables. Make sure that we don't create a Singular matrix. 
+
+y_tr = y[:900]
+X_tr = X.irow(range(900))
+y_test = y[900:] #Not a list
+X_test = X.irow(range(900,1000))
+
+logger = sm.Logit(y,X)
+logger = sm.Logit(y_tr,X_tr)
+logger = logger.fit()
+
+yhat = logger.predict() #returns a bunch of values between 0 and 1. 
+
 loggerfit = logger.fit()
- 
+
 # A function to calculate misclassification rates
  
-def logpredict(logfit,y):
-	y_hat = map(round,logfit.predict())
+def logpredict(logfit,newX,y):
+	y_hat = map(round,logfit.predict(newX))
 	misclass = y -y_hat
-	falsepos = float(sum(misclass==1))/float(len(misclass))
-	falseneg = float(sum(misclass==-1))/float(len(misclass))
-	return([falsepos,falseneg])
+	length = len(y)
+	falsepos = float(sum(misclass==1))/length
+	falseneg = float(sum(misclass==-1))/length
+	#False pos: thought they were class, now they're not
+	#False neg: thought they weren't class, but they were!
+	return(falsepos,falseneg)
 
+def kfold_cv_pandas(X,K,randomise = True):
+	""" kfold cv algorithm adapted for use with PANDAS data conventions
+	note that the outcome var y must be the 1st column of input X   """
+	lenx = len(X) #len() operator works even for pandas data frames
+	rows = random.sample(X.index,lenx)
+	X = X.ix[rows] #this permutes all rows. 
+	for k in xrange(K):
+		training = X.irow([i for i in xrange(lenx) if i % K !=k])
+		validation = X.irow([i for i in xrange(lenx) if i % K == k])
+		yield training, validation
+		
+		
+#So, now we have a returned generator object. We want to find error rate
+
+
+def CV_fit(yX,n,randomize = True):
+	firsttry = kfold_cv_pandas(yX,n,randomize)
+	for training, validation in firsttry:
+		y = training[training.columns[0]]
+		X = training[training.columns[5:]]
+		logger = sm.Logit(y,X)
+		fitted = logger.fit()
+		yield(logpredict(fitted,X,y))
+
+
+##To do: this function is extremely wasteful because you call subplot SO MANY DAMN TIMES!
+Ks = [10,20,30,40,50]
+k_cv,fp_cv,fn_cv = [],[],[]
+for k in Ks:
+	CVresults = CV_fit(s,k)
+	for fp, fn in CVresults:
+		k_cv.append(k)
+		fp_cv.append(fp)
+		fn_csv.append(fn)
+pylab.figure(1)
+pylab.subplot(211)
+pylab.xlim(Ks[0]-2,Ks[-1]+2)
+pylab.ylim([.05,.07])
+pylab.plot(k_cv,fp_cv,'bo')
+pylab.subplot(212)
+pylab.xlim(Ks[0]-2,Ks[-1]+2)
+pylab.ylim(.0,.01)
+pylab.plot(k_cv,fn_cv,'g^')
+pylab.show()
+
+		
+#[X,y] = k_fold_cross_validation(s,2
+	
 #Test:
 logpredict(loggerfit,y)
-	
-# fit the model
+
+
+
+#Next: k-fold cross-validation. 
+
