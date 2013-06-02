14eff61f4f5057e781078c92fcc1acaf57376115
Andy Liu <andyliu1220@gmail.com>
2013-06-02 02:55:17 -0500

    zzz...

--- MRJob_Test/test_mr.py
+++ MRJob_Test/test_mr.py
@@ -4,13 +4,13 @@ class MRTerribleTerribleDamage(MRJob):
 	
 	def mapper(self, _, line):
 		for l in line:
-			yield (l, int(l) ** 2)
+			yield (int(l), int(l) ** 2)
 	
 	def combiner(self, l, squared):
-		yield (l, squared)
+		yield (int(l), squared[0])
 	
 	def reducer(self, l, squared):
-		yield (l, squared)
+		yield (int(l), squared[0])
 	
 if __name__ == '__main__':
 	MRTerribleTerribleDamage.run()
