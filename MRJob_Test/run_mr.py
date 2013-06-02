953ab441193c28a941e1e83f14196a07a89ea036
Andy Liu <andyliu1220@gmail.com>
2013-06-02 02:41:57 -0500

    More typos

--- MRJob_Test/run_mr.py
+++ MRJob_Test/run_mr.py
@@ -2,7 +2,7 @@ from test_mr import MRTerribleTerribleDamage
 import sys
 
 if __name__ == '__main__':
-	job = MRTerribleTerribleDamage(args = argv[1:])
+	job = MRTerribleTerribleDamage(args = sys.argv[1:])
 	with job.make_runner() as runner:
 		runner.run()
 		
