23a99f826e7fe9fcd74629f77246aafda3729bee
Andy Liu <andyliu1220@gmail.com>
2013-06-02 17:28:09 -0500

    More mrjob tests

--- MRJob_Test/mrjob_uniqval.py
+++ MRJob_Test/mrjob_uniqval.py
@@ -3,15 +3,18 @@ from mrjob.job import MRJob
 class MRUniqueVal(MRJob):
 	
 	def mapper(self, _, line):
-		import urllib2, pickle, pandas
-		url = 'https://s3.amazonaws.com/cs12300-spr13-aliu/data/pickled_data'
-		p_data = urllib2.urlopen(url).read()
-		data = pickle.loads(p_data)
+		#import urllib2, pickle, pandas
+		#url = 'https://s3.amazonaws.com/cs12300-spr13-aliu/data/pickled_data'
+		#p_data = urllib2.urlopen(url).read()
+		#data = pickle.loads(p_data)
+		
+		wtf = 0
 		
 		for l in line:
+			wtf += 1
 			var = l.strip().upper()
 			#rv = list(set(data[var]))
-			yield (1, (var, 'hi'))
+			yield (1, (var, 'hi: ' + str(wtf)))
 	
 	def reducer(self, key, value):
 		yield (key, list(value))
